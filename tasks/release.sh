973ada3ef2c8377e12e513f93460c9c80a4a4cf3
Igor Fedorov <iamfedoroff@yandex.ru>
2020-04-20 19:07:13 +0300

    Fix release script

--- tasks/release.sh
+++ tasks/release.sh
@@ -3,6 +3,9 @@
 if [ ! $1 ]; then
   echo "Error: you should pass version number"
 else
+  echo "[vk-api-schema release]: creating version"
+  yarn version --no-git-tag-version --new-version $1
+
   echo "[vk-api-schema release]: commit"
   git add -A && git commit -m "v$1"
 
