c03b3ccc4f1bc73b56934f967ed704a54749865e
Igor Fedorov <iamfedoroff@yandex.ru>
2020-04-20 19:06:19 +0300

    v5.116.1

---
+++ tasks/release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ ! $1 ]; then
+  echo "Error: you should pass version number"
+else
+  echo "[vk-api-schema release]: commit"
+  git add -A && git commit -m "v$1"
+
+  echo "[vk-api-schema release]: add tag"
+  git tag -a "v$1" -m "v$1"
+
+  echo "[vk-api-schema release]: pushing updates"
+  git push origin master
+
+  echo "[vk-api-schema release]: pushing new tag"
+  git push origin "v$1"
+fi
