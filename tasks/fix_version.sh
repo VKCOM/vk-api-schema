3ac1540891c667104369bb54161d6d45bab63aec
Alexey Ashurok <a.ashurok@corp.vk.com>
2021-02-19 19:06:32 +0300

    Add fix version script

---
+++ tasks/fix_version.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+if [ ! "$2" ]; then
+  echo "Error: you should pass API_VERSION version number and BUILD_NUMBER as params"
+else
+  yarn version --no-git-tag-version --new-version "$1.$2"
+  sed -i '' -E 's!([0-9]+\.[0-9]+)!'"$1"'!' README.md
+fi
