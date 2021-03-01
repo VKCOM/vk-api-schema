7a1866dbf6a0f61dbcc055cf076df96ab8d912aa
Alexey Ashurok <work@aotd.ru>
2021-03-01 20:29:45 +0300

    Fix version script

--- tasks/fix_version.sh
+++ tasks/fix_version.sh
@@ -4,5 +4,5 @@ if [ ! "$2" ]; then
   echo "Error: you should pass API_VERSION version number and BUILD_NUMBER as params"
 else
   yarn version --no-git-tag-version --new-version "$1.$2"
-  sed -i '' -E 's!([0-9]+\.[0-9]+)!'"$1"'!' README.md
+  sed -i.bak -E 's!([0-9]+\.[0-9]+)!'"$1"'!' -- README.md && rm -- README.md.bak
 fi
